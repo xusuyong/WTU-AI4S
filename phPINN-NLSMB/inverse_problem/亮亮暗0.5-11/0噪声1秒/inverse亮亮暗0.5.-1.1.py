5b4d8e12298fcecaf69b83a87ab79fac31858d6b
yacdian1 <2315053023@mail.wtu.edu.cn>
2023-12-28 14:27:22 +0800

    PINN逆问题

--- phPINN-NLSMB/inverse_problem/亮亮暗0.5-11/0噪声1秒/inverse亮亮暗0.5.-1.1.py
+++ phPINN-NLSMB/inverse_problem/亮亮暗0.5-11/0噪声1秒/inverse亮亮暗0.5.-1.1.py
@@ -1,18 +1,42 @@
 """Backend supported: tensorflow.compat.v1, tensorflow, pytorch, paddle"""
 import os
 
-os.environ["DDEBACKEND"] = "tensorflow.compat.v1"
+os.environ["DDEBACKEND"] = "pytorch"
+os.makedirs("model", exist_ok=True)
 import deepxde as dde
 import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
+from numpy import exp, cos, sin, log, tanh, cosh, real, imag, sinh, sqrt, arctan
+
 import re
-import scipy.io  # python读取.mat数据之scipy.io&h5py
 import time
 
 start_time = time.time()
-# from deepxde.backend import tf
+if dde.backend.backend_name == "paddle":
+    import paddle
 
+    sin_tesnor = paddle.sin
+    exp_tensor = paddle.exp
+    cos_tesnor = paddle.cos
+    cosh_tensor = paddle.cosh
+    concat = paddle.concat
+elif dde.backend.backend_name == "pytorch":
+    import torch
+
+    sin_tesnor = torch.sin
+    cos_tesnor = torch.cos
+    exp_tensor = torch.exp
+    cosh_tensor = torch.cosh
+    concat = torch.cat
+else:
+    from deepxde.backend import tf
+
+    sin_tesnor = tf.sin
+    cos_tesnor = tf.cos
+    exp_tensor = tf.exp
+    cosh_tensor = tf.math.cosh
+    concat = tf.concat
 
 z_lower = -1
 z_upper = 1
@@ -30,39 +54,35 @@ nt = 512
 x = np.linspace(z_lower, z_upper, nx)[:, None]
 t = np.linspace(t_lower, t_upper, nt)[:, None]
 X, T = np.meshgrid(x, t)
-I = 1j
-EExact = 2 * np.exp(-2 * I / 5 * (5 * T - 2 * X)) / np.cosh(2 * T + (22 * X) / 5)
-pExact = (
-    -I
-    / 5
-    * np.exp(-2 * I / 5 * (5 * T - 2 * X))
-    * (
-        (-2 + I) * np.exp(-2 * T - (22 * X) / 5)
-        - (2 + I) * np.exp(2 * T + (22 * X) / 5)
-    )
-    / np.cosh(2 * T + (22 * X) / 5) ** 2
-)
-etaExact = (5 * np.cosh(2 * T + (22 * X) / 5) ** 2 - 2) / (
-    5 * np.cosh(2 * T + (22 * X) / 5) ** 2
-)
-EExact_u = np.real(EExact)  # (201,256)
-EExact_v = np.imag(EExact)
-pExact_u = np.real(pExact)
-pExact_v = np.imag(pExact)
-etaExact_u = np.real(etaExact)  # (201,256)
 X_star = np.hstack((X.flatten()[:, None], T.flatten()[:, None]))
+I = 1j
 
-space_domain = dde.geometry.Interval(z_lower, z_upper)  # 先定义空间
-time_domain = dde.geometry.TimeDomain(t_lower, t_upper)  # 再定义时间
-geomtime = dde.geometry.GeometryXTime(space_domain, time_domain)  # 结合一下，变成时空区域
+alpha_1 = dde.Variable(1.0)
+alpha_2 = dde.Variable(2.0)
+omega_0 = dde.Variable(0.0)
 
-lambda1 = dde.Variable(2.0)  # dtype = float64
-lambda2 = dde.Variable(2.0)
-omega = dde.Variable(2.0)
-True_lambda1 = 0.5  # dtype = float64
-True_lambda2 = -1
-True_omega = 1
+true_var_dict = {"alpha_1": 0.5, "alpha_2": -1, "omega_0": 0.5}
+var_list = [alpha_1, alpha_2, omega_0]
+"""X = x[:, 0:1]不能变成X = x[:, 0]!!!"""
 
+def solution(XT):
+    X = XT[:, 0:1]
+    T = XT[:, 1:2]
+    EExact = 2 * np.exp(-2 * I / 5 * (5 * T - 2 * X)) / np.cosh(2 * T + (22 * X) / 5)
+    pExact = (
+            -I
+            / 5
+            * np.exp(-2 * I / 5 * (5 * T - 2 * X))
+            * (
+                    (-2 + I) * np.exp(-2 * T - (22 * X) / 5)
+                    - (2 + I) * np.exp(2 * T + (22 * X) / 5)
+            )
+            / np.cosh(2 * T + (22 * X) / 5) ** 2
+    )
+    etaExact = (5 * np.cosh(2 * T + (22 * X) / 5) ** 2 - 2) / (
+            5 * np.cosh(2 * T + (22 * X) / 5) ** 2
+    )
+    return real(EExact), imag(EExact), real(pExact), imag(pExact), etaExact
 
 def pde(x, y):  # 这里x其实是x和t，y其实是u和v
     """
@@ -95,88 +115,25 @@ def pde(x, y):  # 这里x其实是x和t，y其实是u和v
     Eu_tt = dde.grad.hessian(y, x, component=0, i=1, j=1)
     Ev_tt = dde.grad.hessian(y, x, component=1, i=1, j=1)
 
-    f1_u = lambda1 * Eu_tt - lambda2 * Eu * (Eu**2 + Ev**2) + 2 * pv - Ev_z
-    f1_v = lambda1 * Ev_tt - lambda2 * Ev * (Eu**2 + Ev**2) - 2 * pu + Eu_z
-    f2_u = 2 * Ev * eta - pv_t + 2 * pu * omega
-    f2_v = -2 * Eu * eta + pu_t + 2 * pv * omega
+    f1_u = alpha_1 * Eu_tt - alpha_2 * Eu * (Eu**2 + Ev**2) + 2 * pv - Ev_z
+    f1_v = alpha_1 * Ev_tt - alpha_2 * Ev * (Eu**2 + Ev**2) - 2 * pu + Eu_z
+    f2_u = 2 * Ev * eta - pv_t + 2 * pu * omega_0
+    f2_v = -2 * Eu * eta + pu_t + 2 * pv * omega_0
     f3 = 2 * pv * Ev + 2 * pu * Eu + eta_t
 
     return [f1_u, f1_v, f2_u, f2_v, f3]
 
+EExact_u, EExact_v, pExact_u, pExact_v, etaExact_u = solution(X_star)
 
-# x=x[:, 0:1]
-# t=x[:, 1:2]**
-"""精确解"""
-I = 1j
+space_domain = dde.geometry.Interval(z_lower, z_upper)  # 先定义空间
+time_domain = dde.geometry.TimeDomain(t_lower, t_upper)  # 再定义时间
+geomtime = dde.geometry.GeometryXTime(space_domain, time_domain)  # 结合一下，变成时空区域
 
+"""inverse"""
+idx = np.random.choice(X_star.shape[0], 5000, replace=False)
+X_u_train = X_star[idx, :]
 
-def Eu_func(x):
-    return (
-        2
-        * np.cos(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-        / np.cosh(2 * x[:, 1:2] + (22 * x[:, 0:1]) / 5)
-    )
-
-
-def Ev_func(x):
-    return (
-        -2
-        * np.sin(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-        / np.cosh(2 * x[:, 1:2] + (22 * x[:, 0:1]) / 5)
-    )
-
-
-def pu_func(x):
-    return (
-        (
-            np.cos(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-            + 2 * np.sin(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-        )
-        * np.exp(-2 * x[:, 1:2] - (22 * x[:, 0:1]) / 5)
-        - np.exp(2 * x[:, 1:2] + (22 * x[:, 0:1]) / 5)
-        * (
-            np.cos(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-            - 2 * np.sin(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-        )
-    ) / (5 * np.cosh(2 * x[:, 1:2] + (22 * x[:, 0:1]) / 5) ** 2)
-
-
-def pv_func(x):
-    return (
-        (
-            2 * np.cos(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-            - np.sin(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-        )
-        * np.exp(-2 * x[:, 1:2] - (22 * x[:, 0:1]) / 5)
-        + 2
-        * (
-            np.cos(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5)
-            + np.sin(2 * x[:, 1:2] - (4 * x[:, 0:1]) / 5) / 2
-        )
-        * np.exp(2 * x[:, 1:2] + (22 * x[:, 0:1]) / 5)
-    ) / (5 * np.cosh(2 * x[:, 1:2] + (22 * x[:, 0:1]) / 5) ** 2)
-
-
-def eta_func(x):
-    return (5 * np.cosh(2 * x[:, 1:2] + (22 * x[:, 0:1]) / 5) ** 2 - 2) / (
-        5 * np.cosh(2 * x[:, 1:2] + (22 * x[:, 0:1]) / 5) ** 2
-    )
-
-
-idx = np.random.choice(X_star.shape[0], 2000, replace=False)
-X_u_train = X_star[idx, :]  # (2000,2)
-EExact_u_1d = Eu_func(X_u_train)
-EExact_v_1d = Ev_func(X_u_train)
-pExact_u_1d = pu_func(X_u_train)
-pExact_v_1d = pv_func(X_u_train)
-etaExact_u_1d = eta_func(X_u_train)
-
-# noise = 0.05
-# EExact_u_1d = EExact_u_1d + noise*np.std(EExact_u_1d)*np.random.randn(EExact_u_1d.shape[0], EExact_u_1d.shape[1])
-# EExact_v_1d = EExact_v_1d + noise*np.std(EExact_v_1d)*np.random.randn(EExact_v_1d.shape[0], EExact_v_1d.shape[1])
-# pExact_u_1d = pExact_u_1d + noise*np.std(pExact_u_1d)*np.random.randn(pExact_u_1d.shape[0], pExact_u_1d.shape[1])
-# pExact_v_1d = pExact_v_1d + noise*np.std(pExact_v_1d)*np.random.randn(pExact_v_1d.shape[0], pExact_v_1d.shape[1])
-# etaExact_u_1d = etaExact_u_1d + noise*np.std(etaExact_u_1d)*np.random.randn(etaExact_u_1d.shape[0], etaExact_u_1d.shape[1])
+EExact_u_1d, EExact_v_1d, pExact_u_1d, pExact_v_1d, etaExact_u_1d = solution(X_u_train)
 
 observe_y = dde.icbc.PointSetBC(X_u_train, EExact_u_1d, component=0)
 observe_y1 = dde.icbc.PointSetBC(X_u_train, EExact_v_1d, component=1)
@@ -187,61 +144,126 @@ observe_y4 = dde.icbc.PointSetBC(X_u_train, etaExact_u_1d, component=4)
 data = dde.data.TimePDE(
     geomtime,
     pde,
+    # [],
     [observe_y, observe_y1, observe_y2, observe_y3, observe_y4],
-    num_domain=10000,
+    num_domain=20000,
     # num_boundary=20,
     anchors=X_u_train,
-    # solution=func,
+    solution=lambda XT: np.hstack((solution(XT))),
     # num_test=10000,
 )
 
-layer_size = [2] + [64] * 6 + [5]
-activation = "tanh"
-initializer = "Glorot uniform"
-net = dde.nn.FNN(layer_size, activation, initializer)
+layer_size = [2] + [128] * 4 + [5]
 
+net = dde.nn.FNN(layer_size, "sin", "Glorot uniform")
+
+
+def feature_transform(XT):
+    z = XT[:, 0:1]
+    t = XT[:, 1:2]
+    return concat(
+        [(z - z_lower) / (z_upper - z_lower), (t - t_lower) / (t_upper - t_lower)], 1
+    )
+
+net.apply_feature_transform(feature_transform)
+
+def output_transform(XT, y):
+    Eu = y[:, 0:1]
+    Ev = y[:, 1:2]
+    pu = y[:, 2:3]
+    pv = y[:, 3:4]
+    eta = y[:, 4:5]
+    X = XT[:, 0:1]
+    T = XT[:, 1:2]
+    cos = cos_tesnor
+    sin = sin_tesnor
+    cosh = cosh_tensor
+    exp = exp_tensor
+
+    EExact = 2 * exp(-2 * I / 5 * (5 * T - 2 * X)) / cosh(2 * T + (22 * X) / 5)
+    pExact = (
+            -I
+            / 5
+            * exp(-2 * I / 5 * (5 * T - 2 * X))
+            * (
+                    (-2 + I) * exp(-2 * T - (22 * X) / 5)
+                    - (2 + I) * exp(2 * T + (22 * X) / 5)
+            )
+            / cosh(2 * T + (22 * X) / 5) ** 2
+    )
+    etaExact = (5 * cosh(2 * T + (22 * X) / 5) ** 2 - 2) / (
+            5 * cosh(2 * T + (22 * X) / 5) ** 2
+    )
+    Eu_true = torch.real(EExact)
+    Ev_true = torch.imag(EExact)
+    pu_true = torch.real(pExact)
+    pv_true = torch.imag(pExact)
+
+    # aaa = (1 - torch.exp(z - z_upper)) * (1 - torch.exp(z_lower - z)) * (1 - torch.exp(t_lower - t)) * Uu + Eu_true
+    # bbb = (1 - torch.exp(z - z_upper)) * (1 - torch.exp(z_lower - z)) * (1 - torch.exp(t_lower - t)) * Uv + Ev_true
+    # ccc = (1 - torch.exp(z - z_upper)) * (1 - torch.exp(z_lower - z)) * (1 - torch.exp(t_lower - t)) * Vu + pu_true
+    # ddd = (1 - torch.exp(z - z_upper)) * (1 - torch.exp(z_lower - z)) * (1 - torch.exp(t_lower - t)) * Vv + pv_true
+    # eee = (1 - torch.exp(z - z_upper)) * (1 - torch.exp(z_lower - z)) * (1 - torch.exp(t_lower - t)) * phi0u + phi0u_true
+    # fff = (1 - torch.exp(z - z_upper)) * (1 - torch.exp(z_lower - z)) * (1 - torch.exp(t_lower - t)) * phi0v + phi0v_true
+
+    # return concat([aaa, bbb, ccc, ddd, eee, fff], dim=1)
+    return concat([Eu_true, Ev_true, pu_true, pv_true, etaExact], 1)
+
+
+# net.apply_output_transform(output_transform)
 model = dde.Model(data, net)
 
+iterations = 4000
+loss_weights = [1, 1, 1, 1, 1, 100, 100, 100, 100, 100]
 model.compile(
     "adam",
     lr=0.001,
-    loss_weights=[1, 1, 1, 1, 1, 100, 100, 100, 100, 100],
-    # decay=("inverse time", 5000, 0.6),
-    external_trainable_variables=[lambda1, lambda2, omega],
+    loss_weights=loss_weights,
+    metrics=["l2 relative error"],
+    decay=("inverse time", iterations // 5, 0.5),
+    external_trainable_variables=var_list,
 )
 filenamevar = "variables1.dat"
 variable = dde.callbacks.VariableValue(
-    [lambda1, lambda2, omega],
-    period=period,
-    filename=filenamevar,  # 加了filename就不会打印了
-    precision=7,
+    var_list, period=period, filename=filenamevar, precision=7  # 加了filename就不会打印了
+)
+"""Resample"""
+resampler = dde.callbacks.PDEPointResampler(
+    period=2000, pde_points=True, bc_points=True
 )
 losshistory, train_state = model.train(
-    iterations=20000, display_every=100, callbacks=[variable]
+    iterations=iterations,
+    display_every=100,
+    model_save_path="model/",
+    callbacks=[resampler, variable],
 )
 
-# dde.optimizers.config.set_LBFGS_options(
-#     maxcor=50,
-#     ftol=1.0 * np.finfo(float).eps,
-#     gtol=1e-08,
-#     maxiter=2000,
-#     maxfun=None,
-#     maxls=50,
-# )
-model.compile(
-    "L-BFGS",
-    # loss_weights=[1, 1, 1, 1, 1, 1, 100, 100, 100, 100, 100, 100],
-    external_trainable_variables=[lambda1, lambda2, omega],
-)
-losshistory, train_state = model.train(display_every=100, callbacks=[variable])
+if 1:
+    dde.optimizers.config.set_LBFGS_options(
+        # maxcor=50,
+        # ftol=1.0 * np.finfo(float).eps,
+        # gtol=1e-08,
+        maxiter=1000,
+        # maxfun=None,
+        # maxls=50,
+    )
+    model.compile(
+        "L-BFGS",
+        loss_weights=loss_weights,
+        metrics=["l2 relative error"],
+        external_trainable_variables=var_list,
+    )
+    losshistory, train_state = model.train(
+        display_every=100, model_save_path="model/", callbacks=[resampler, variable]
+    )
+
+# model.restore('model/-205.ckpt')
 
 """精确解"""
-EExact_h = np.abs(EExact)  # （201，256）
-Eh_true = EExact_h.flatten()  # (51456,)
-pExact_h = np.abs(pExact)
-ph_true = pExact_h.flatten()
-etaExact_h = np.abs(etaExact)
-etah_true = etaExact_h.flatten()
+Eh_true = np.sqrt(EExact_u**2 + EExact_v**2).flatten()
+ph_true = np.sqrt(pExact_u**2 + pExact_v**2).flatten()
+etah_true = np.abs(etaExact_u).flatten()
+
 prediction = model.predict(
     X_star, operator=None
 )  # 如果 `operator` 为 `None`，则返回网络输出，否则返回 `operator` 的输出
@@ -260,6 +282,10 @@ print("E L2 relative error: %e" % E_L2_relative_error)
 print("p L2 relative error: %e" % p_L2_relative_error)
 print("eta L2 relative error: %e" % eta_L2_relative_error)
 elapsed = time.time() - start_time
+
+EExact_h = Eh_true.reshape(nt, nx)
+pExact_h = ph_true.reshape(nt, nx)
+etaExact_h = etah_true.reshape(nt, nx)
 """预测解"""
 EH_pred = Eh_pred.reshape(nt, nx)
 pH_pred = ph_pred.reshape(nt, nx)
@@ -270,20 +296,30 @@ lines = open(filenamevar, "r").readlines()
 Chat = np.array(
     [
         np.fromstring(
-            min(re.findall(re.escape("[") + "(.*?)" + re.escape("]"), line), key=len),
+            min(re.findall(re.escape("[") + "(.*?)" + re.escape("]"), line)),
             sep=",",
         )
         for line in lines
     ]
 )
 
-error_lambda_1 = abs((True_lambda1 - Chat[-1, 0]) / True_lambda1) * 100
-error_lambda_2 = abs((True_lambda2 - Chat[-1, 1]) / True_lambda2) * 100
-error_omega = abs((True_omega - Chat[-1, 2]) / True_omega) * 100
-
-print("Error l1: %.5f%%" % (error_lambda_1))
-print("Error l2: %.5f%%" % (error_lambda_2))
-print("Error o: %.5f%%" % (error_omega))
+l, c = Chat.shape
+i = 0
+plt.figure("反问题系数", dpi=dpi, facecolor=None, edgecolor=None)
+for key, value in true_var_dict.items():
+    relative_error = dde.metrics._absolute_percentage_error(value, Chat[-1, i])
+    print("relative_error_{}: {}".format(key, relative_error))
+    plt.plot(
+        range(0, period * l, period),
+        np.ones(Chat[:, 0].shape) * value,
+        "--",
+        label="true $\\" + key + "$",
+    )
+    plt.plot(range(0, period * l, period), Chat[:, i], label="$\\" + key + "$")
+    i += 1
+plt.legend()
+plt.xlabel("iterations")
+plt.savefig("反问题系数.png")
 
 fig5 = plt.figure("3d预测演化图E", dpi=dpi)
 ax = fig5.add_subplot(projection="3d")
@@ -309,9 +345,9 @@ surf = ax.plot_surface(
     X,
     T,
     pH_pred,
-    rstride=stride,  # 指定行的跨度
-    cstride=stride,  # 指定列的跨度
-    cmap="Spectral",  # 设置颜色映射
+    rstride=stride,
+    cstride=stride,
+    cmap="Spectral",
     linewidth=0,
     antialiased=False,
 )  # 抗锯齿
@@ -566,50 +602,13 @@ h2 = ax2.imshow(
 fig17.colorbar(h0, ax=[ax0, ax1, ax2], location="right")
 # plt.subplots_adjust(left=0.15, right=1-0.01,bottom=0.08, top=1-0.08,wspace=None, hspace=0.25)
 # plt.tight_layout()#自动调整大小和间距，使各个子图标签不重叠
-# fnamevar = r"F:\QQ文件\NLS-MB项目\dde\inverse_problem\variables.dat"
-fig = plt.figure("反问题系数", dpi=dpi, facecolor=None, edgecolor=None)
-
-l, c = Chat.shape
-plt.plot(range(0, period * l, period), Chat[:, 0], "r-")
-plt.plot(range(0, period * l, period), Chat[:, 1], "k-")
-plt.plot(range(0, period * l, period), Chat[:, 2], "g-")
-plt.plot(range(0, period * l, period), np.ones(Chat[:, 0].shape) * True_lambda1, "r--")
-plt.plot(range(0, period * l, period), np.ones(Chat[:, 1].shape) * True_lambda2, "k--")
-plt.plot(range(0, period * l, period), np.ones(Chat[:, 2].shape) * True_omega, "g--")
-plt.legend(
-    [
-        "$\lambda_1$",
-        "$\omega$",
-        "$\lambda_2$",
-        "True $\lambda_1$",
-        "True $\omega$",
-        "True $\lambda_2$",
-    ]
-)
-plt.xlabel("iterations")
-# plt.tight_layout()
 
 
-dde.saveplot(losshistory, train_state, issave=True, isplot=True)
-scipy.io.savemat(
-    "反问题亮亮暗0的噪声0.5,-1,1.mat",
-    {
-        "x": x,
-        "t": t,
-        "elapsed": elapsed,
-        "X_u_train": X_u_train,
-        "E_L2_relative_error": E_L2_relative_error,
-        "p_L2_relative_error": p_L2_relative_error,
-        "eta_L2_relative_error": eta_L2_relative_error,
-        "EH_pred": EH_pred,
-        "pH_pred": pH_pred,
-        "etaH_pred": etaH_pred,
-        "EExact_h": EExact_h,
-        "pExact_h": pExact_h,
-        "etaExact_h": etaExact_h,
-        "error_lambda_1": error_lambda_1,
-        "error_lambda_2": error_lambda_2,
-        "error_omega": error_omega,
-    },
-)
+dde.saveplot(losshistory, train_state, issave=True, isplot=True, output_dir="model/")
+# scipy.io.savemat('bound-state.mat',
+#                  {'x': x, 't': t, 'elapsed': elapsed, 'X_u_train': X_u_train,
+#                   'U_L2_relative_error': U_L2_relative_error, 'V_L2_relative_error': V_L2_relative_error,
+#                   'phi0_L2_relative_error': phi0_L2_relative_error,
+#                   'UH_pred': UH_pred, 'VH_pred': VH_pred, 'phi0H_pred': phi0H_pred,
+#                   'UExact_h': UExact_h, 'VExact_h': VExact_h, 'phi0Exact_h': phi0Exact_h})
 plt.show()
