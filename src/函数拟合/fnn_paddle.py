7d6e0540542fd6c9e02a69d2c894f653a8d4fd17
xusuyong <2209245477@qq.com>
2024-05-07 09:50:36 +0800

    简化代码

--- src/函数拟合/fnn_paddle.py
+++ src/函数拟合/fnn_paddle.py
@@ -1,18 +1,19 @@
 import paddle
+import paddle.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-class FNN(paddle.nn.Layer):
+class FNN(nn.Layer):
     def __init__(self, layer_sizes):
         super(FNN, self).__init__()
-        self.linears = paddle.nn.LayerList()
+        self.linears = nn.LayerList()
         for i in range(1, len(layer_sizes)):
-            self.linears.append(paddle.nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
+            self.linears.append(nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
 
     def forward(self, x):
         for linear in self.linears[:-1]:
-            x = paddle.nn.functional.relu(linear(x))
+            x = nn.functional.relu(linear(x))
         x = self.linears[-1](x)
         return x
 
@@ -21,25 +22,28 @@ train_data = np.loadtxt("train.txt").astype(np.float32)
 train_x, train_y = train_data[:, :1], train_data[:, 1:]
 test_data = np.loadtxt("test.txt").astype(np.float32)
 test_x, test_y = test_data[:, :1], test_data[:, 1:]
+
 layer_sizes = [1] + [128] * 4 + [1]
 lr = 0.001
-nsteps = 10000
-nn = FNN(layer_sizes)
+nsteps = 5000
+net = FNN(layer_sizes)
 optimizer = paddle.optimizer.Adam(
-    parameters=nn.parameters(), learning_rate=lr, weight_decay=0.0
+    parameters=net.parameters(), learning_rate=lr, weight_decay=0.0
 )
+
 for i in range(nsteps):
-    y = nn(paddle.to_tensor(train_x))
-    loss_fn = paddle.nn.MSELoss()
+    y = net(paddle.to_tensor(train_x))
+    loss_fn = nn.MSELoss()
     loss = loss_fn(y, paddle.to_tensor(train_y))
     optimizer.clear_grad()
     loss.backward()
     optimizer.step()
     if i % 1000 == 0 or i == nsteps - 1:
         with paddle.no_grad():
-            pred_y = nn(paddle.to_tensor(test_x)).detach().cpu().numpy()
+            pred_y = net(paddle.to_tensor(test_x)).detach().cpu().numpy()
         err_test = np.mean((pred_y - test_y) ** 2)
         print(i, loss.item(), err_test)
+plt.figure()
 plt.plot(test_x, test_y, "o")
 plt.plot(test_x, pred_y, "v")
 plt.show()
--- src/函数拟合/fnn_pytorch.py
+++ src/函数拟合/fnn_pytorch.py
@@ -1,18 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import torch.nn as nn
 
-
-class FNN(torch.nn.Module):  # 继承torch.nn.Module
+class FNN(nn.Module):  # 继承torch.nn.Module
     def __init__(self, layer_sizes):
         super(FNN, self).__init__()
-        self.linears = torch.nn.ModuleList()
+        self.linears = nn.ModuleList()
         for i in range(1, len(layer_sizes)):
-            self.linears.append(torch.nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
+            self.linears.append(nn.Linear(layer_sizes[i - 1], layer_sizes[i]))
 
     def forward(self, x):
         for linear in self.linears[:-1]:
-            x = torch.nn.functional.relu(linear(x))
+            x = nn.functional.relu(linear(x))
         x = self.linears[-1](x)
         return x
 
@@ -26,19 +26,19 @@ test_x, test_y = test_data[:, :1], test_data[:, 1:]
 # Hyperparameters
 layer_sizes = [1] + [128] * 4 + [1]
 lr = 0.001
-nsteps = 10000
+nsteps = 5000
 
 # Build NN
-nn = FNN(layer_sizes)
+net = FNN(layer_sizes)
 
 # Optimizer
-optimizer = torch.optim.Adam(nn.parameters(), lr=lr)
+optimizer = torch.optim.Adam(net.parameters(), lr=lr)
 
 # Train
 for i in range(nsteps):
-    y = nn(torch.from_numpy(train_x))  # 手动将numpy转成tensor
+    y = net(torch.from_numpy(train_x))  # 手动将numpy转成tensor
     # Loss
-    loss_fn = torch.nn.MSELoss()
+    loss_fn = nn.MSELoss()
     loss = loss_fn(y, torch.from_numpy(train_y))  # 预测值和真值
     optimizer.zero_grad()  # 清零所有参数的梯度，以便进行下一轮反向传播之前不累积梯度。
     loss.backward()  # 反向传播，计算损失函数关于各个参数的梯度。
@@ -46,7 +46,7 @@ for i in range(nsteps):
 
     if i % 1000 == 0 or i == nsteps - 1:
         with torch.no_grad():  # 进入一个上下文管理器，表示在该代码块中不需要计算梯度。
-            pred_y = nn(torch.from_numpy(test_x)).detach().cpu().numpy()
+            pred_y = net(torch.from_numpy(test_x)).detach().cpu().numpy()
         err_test = np.mean((pred_y - test_y) ** 2)
         print(i, loss.item(), err_test)
 
